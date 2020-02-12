2469be4285e0ce8492d87d49943e90b39ca5e8c1
trallerd <jessicacristec@gmail.com>
2020-02-12 11:02:59 -0300

    tarefa web II

---
+++ CadastrarPessoa.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Cadastrar Pessoa fisica</title>
+</head>
+<body>
+    <h1>Cadastrar Pessoa fisica</h1>
+
+    <form action="/pagina-processa-dados-do-form" method="post">
+
+        <div>
+            <label for="nome">Nome:</label>
+            <input type="text" name = "Nome"/>
+        </div>
+        <div>
+            <label for="cpf">CPF:</label>
+            <input type="text" name = "CPF" />
+        </div>
+        <div>
+            <label for="ende">Endereço:</label>
+            <input type="text" name = "Endereço" />
+        </div>
+        <div>
+            <label for="tel">Telefone:</label>
+            <input type="text" name = "Telefone" />
+        </div>
+    </form>
+</body>
+</html>
